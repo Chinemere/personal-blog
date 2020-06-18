d041dc902d2f341cf25d734e7de06147671caaa5
Chinemere Chukwu <chukwuchinemere@gmail.com>
2020-06-18 10:18:46 +0100

    Personal website

---
+++ app.js
@@ -0,0 +1,23 @@
+function followMeMouseOver() {
+    var socialMediaIcons = document.getElementById('social');
+    socialMediaIcons.style.visibility = "visible";
+    var follow = document.getElementById('follow');
+    follow.style.visibility = "hidden";
+
+}
+
+function socialMouseOut() {
+    var socialMediaIcons = document.getElementById('social')
+    socialMediaIcons.style.visibility = "hidden"
+    var follow = document.getElementById('follow')
+    follow.style.visibility = "visible"
+
+}
+
+function socialMouseOver() {
+    var socialMediaIcons = document.getElementById('social');
+    socialMediaIcons.style.visibility = "visible";
+    var follow = document.getElementById('follow');
+    follow.style.visibility = "hidden";
+
+}
