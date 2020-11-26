3bbc0c18edcbb7593ed004970e54f94ddc3d094a
Chinemere chukwu <chukwuchinemere@gmail.com>
2020-11-26 21:56:53 +0100

    theme choice added

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+// Code for changing the theme of the website
+const checkbox = document.getElementById("checkbox")
+checkbox.addEventListener("change", () => {
+    // Change the theme of the website
+    document.body.classList.toggle("dark")
+})
+// End of code
+
+
 function followMeMouseOver() {
     var socialMediaIcons = document.getElementById('social');
     socialMediaIcons.style.visibility = "visible";
@@ -21,3 +30,18 @@ function socialMouseOver() {
     follow.style.visibility = "hidden";
 
 }
+
+var light = true;
+function themeLight() {
+    if (light === false) {
+        document.getElementById("light").style.backgroundColor = "#FCFCFC";
+        document.getElementById("light").style.color = "#777777";
+    }
+
+    else {
+        document.getElementById("light").style.backgroundColor = "#030307";
+        document.getElementById("light").style.color = "#C9D0FA";
+    }
+    light = false;
+}
+
